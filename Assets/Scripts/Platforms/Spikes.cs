96b9d43976b318c4f29c006f5061ab1b05ee3039
jordanmccollam <jordy.mccollam@gmail.com>
2022-02-16 14:39:57 -0600

    Fixed bug jumping onto spiked horizontally

--- Assets/Scripts/Platforms/Spikes.cs
+++ Assets/Scripts/Platforms/Spikes.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
-    public float gravityOnImpact;
-
     bool playerSpiked = false;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {            
             Player playerComp = other.GetComponent<Player>();
-            if (playerComp.isFalling && !playerSpiked) {
-                playerSpiked = true; // So trigger doesn't KEEP triggering
-                playerComp.rb.gravityScale = gravityOnImpact;
+            // If player isn't already spiked, and is falling DOWNWARDS
+            if (!playerSpiked && playerComp.isFalling && playerComp.rb.velocity.y < 0) {
+                playerSpiked = true;
                 playerComp.TakeDamage(playerComp.health);
             }
         }

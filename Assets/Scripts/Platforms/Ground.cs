3fbfccbb75a9faeb17092d422a965b1e9cf6e627
jordanmccollam <jordy.mccollam@gmail.com>
2022-02-18 16:01:05 -0600

    There are never tiles without grass now

---
+++ Assets/Scripts/Platforms/Ground.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public GameObject top;
+    public LayerMask whatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckIfTop();
+    }
+
+    void CheckIfTop() {
+        Vector2 point = new Vector2(transform.position.x, transform.position.y + 1f);
+        Vector2 size = new Vector2(.5f, .5f);
+        Collider2D groundDetection = Physics2D.OverlapBox(point, size, 0, whatIsGround);
+        if (groundDetection == null) {
+            Debug.Log("is top");
+            top.SetActive(true);
+        } else {
+            Debug.Log("is underground");
+            Debug.Log(groundDetection);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector2 point = new Vector2(transform.position.x, transform.position.y + 1f);
+        Vector3 size = new Vector3(.5f, .5f, .5f);
+
+        // Draw a semitransparent blue cube at the transforms position
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(point, size);
+    }
+}
